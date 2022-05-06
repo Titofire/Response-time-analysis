7829286a897485d0bb946cc5e004e94ba89b864f
Titofire <tpoole2163@gmail.com>
2022-05-06 08:33:56 -0400

    R code chunk build .df to filter/select frequent locations, added new .rmd file and .html

--- Response time.R
+++ Response time.R
@@ -10,4 +10,9 @@
         Pivot_Table_2016_2021.df <- read.csv(file = "pivot20162021.csv",header = TRUE, na.strings=c("", "NA"),sep = ",")
         
         
-####   Build new data frame with only colums and rows needed
+####   Build new data frame with only columns and rows needed ####
+        
+        Response_Times.df <- Pivot_Table_2016_2021.df %>%
+                select(inci_id,station,number,street,resp) %>%
+                filter()
+                
