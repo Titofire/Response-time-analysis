7c5fb4b4da280d4174c9e4a4d1cb82c2b49f0dab
Titofire <tpoole2163@gmail.com>
2022-05-09 12:05:07 -0400

    Left Joined pivot tabel with response time and reworked .df's. Data is now only looks at Ladder one and Engine 2, not by sta1 or sta2 as previous.
    Added box plots and hist for exploritory with 1510 & 1530 williston.

--- Response time.R
+++ Response time.R
@@ -9,30 +9,38 @@
 ####   Import data and remove non-essential fields/variables ####
 
                 Pivot_Table_2016_2021.df <- read.csv(file = "pivot20162021.csv",header = TRUE, na.strings=c("", "NA"),sep = ",")
+                Unitresp20102021.df <- read.csv(file = "unitresp20102021.csv",header = TRUE, na.strings=c("", "NA"),sep = ",")
         
+                ## Left join both df above ##
+                
+                        Left_joinedpivotresp.df <- left_join(Pivot_Table_2016_2021.df,Unitresp20102021.df, by = "inci_no")
+                        
 ####   Build new data frame and filter for 465 Quarryhill, Allen road, 1510-1530 Williston, 20 Harborview ####
         
-                Response_Times.df <- Pivot_Table_2016_2021.df %>%
-                        select(inci_id,station,number,street,resp) %>%
+                Frequent_healthcare.df <- Left_joinedpivotresp.df %>%
+                        select(inci_no,station,number,street,resp,unit_desc) %>%
                         filter(number %in% c("90","20","1510","465","310","1530"),street %in% c("Williston","Quarryhill", "Allen", "Anderson","Harborview","Market"))
                 
         ## Now build discrete data frames for each occupancy and explore by apparatus and build box plot graphs ##
                 
                 ## 1510 and 1530 Williston Rd., Ladder1 responses (Gazebo_ladder1.df) ##
                 
-                Gazebo_ladder1.df <- Response_Times.df %>% 
-                       filter(number %in% c("1510","1530"), street %in% c("Williston")))
-                summary(Gazebo_ladder1.df$resp)
+                Gazebo_ladder1.df <- Frequent_healthcare.df %>% 
+                       filter(number %in% c("1510","1530"), street %in% c("Williston"), unit_desc %in% c("Ladder 1"))
+                        summary(Gazebo_ladder1.df$resp)
+                        
+                        qplot(Gazebo_ladder1.df$resp)
+                        boxplot(Gazebo_ladder1.df$resp,main="Ladder 1 response to 1510 or 1530 Williston rd.",xlab = "", ylab = "Seconds")
                 
                 ## 1510 and 1530 Williston Rd., Engine2 responses (Gazebo_engine2.df) ##
                 
-                Gazebo_engine2.df <- Response_Times.df %>% 
-                        filter(number %in% c("1510","1530"), street %in% c("Williston"),
-                summary(Gazebo_station2.df$resp)
+                Gazebo_engine2.df <- Frequent_healthcare.df %>% 
+                        filter(number %in% c("1510","1530"), street %in% c("Williston"), unit_desc %in% c("Engine 2"))
+                        summary(Gazebo_engine2.df$resp)
                 
                 ## 90 Allen, Engine 2 responses (Allen_engine2.df) ##
                 
-                Allen_engine2.df <- Response_Times.df %>% 
-                        filter(number %in% c("1510","1530"), street %in% c("Williston"))
-                summary(Allen_engine2.df$resp)
+                Allen_engine2.df <- Frequent_healthcare.df %>% 
+                        filter(number %in% c("90"), street %in% c("Allen"), unit_desc %in% c("Engine 2"))
+                        summary(Allen_engine2.df$resp)
                 
